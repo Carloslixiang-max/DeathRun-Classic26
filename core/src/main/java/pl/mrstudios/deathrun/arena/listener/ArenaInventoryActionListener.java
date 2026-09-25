6402c113d044a02c3c2bafd52b94245e9f9567a7
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 16:57:04 +0800

    fix: isolate DeathRun listeners to its runtime

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaInventoryActionListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaInventoryActionListener.java
@@ -1,5 +1,6 @@
 package pl.mrstudios.deathrun.arena.listener;
 
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
@@ -15,6 +16,7 @@ import pl.mrstudios.deathrun.arena.ArenaManager;
 
 import static org.bukkit.event.EventPriority.MONITOR;
 import static org.bukkit.event.inventory.InventoryType.PLAYER;
+
 public class ArenaInventoryActionListener implements Listener {
 
     private static final String INVENTORY_BYPASS_PERMISSION = "deathrun.inventory.bypass";
@@ -23,98 +25,66 @@ public class ArenaInventoryActionListener implements Listener {
     private final Plugin plugin;
 
     @Inject
-    public ArenaInventoryActionListener(
-            @NotNull ArenaManager arenaManager,
-            @NotNull Plugin plugin
-    ) {
+    public ArenaInventoryActionListener(@NotNull ArenaManager arenaManager, @NotNull Plugin plugin) {
         this.arenaManager = arenaManager;
         this.plugin = plugin;
     }
 
     @EventHandler(priority = MONITOR)
-    public void onInventoryClick(
-            @NotNull InventoryClickEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getWhoClicked()))
+    public void onInventoryClick(@NotNull InventoryClickEvent event) {
+        if (!(event.getWhoClicked() instanceof Player player))
             return;
-
-        if (event.getClickedInventory() == null)
+        if (!this.inDeathRun(player) || this.hasInventoryBypass(player))
             return;
-
-        if (event.getClickedInventory().getType() != PLAYER)
+        if (event.getClickedInventory() == null || event.getClickedInventory().getType() != PLAYER)
             return;
-
-        event.setCancelled(true);
-
-    }
-
-    @EventHandler(priority = MONITOR)
-    public void onCreativeInventory(
-            @NotNull InventoryCreativeEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getWhoClicked()))
-            return;
-
         event.setCancelled(true);
     }
 
     @EventHandler(priority = MONITOR)
-    public void onItemDrop(
-            @NotNull PlayerDropItemEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getPlayer()))
+    public void onCreativeInventory(@NotNull InventoryCreativeEvent event) {
+        if (!(event.getWhoClicked() instanceof Player player))
             return;
-
-        // Paper 1.20.2+ also fires PlayerDropItemEvent on death.
-        // Do not cancel in that case, otherwise death drops are swallowed.
-        if (event.getPlayer().isDead())
+        if (!this.inDeathRun(player) || this.hasInventoryBypass(player))
             return;
-
-        var runtime = this.arenaManager.runtimeForPlayer(event.getPlayer());
-        if (runtime == null)
-            return;
-
-        event.setCancelled(true);
-
-        this.plugin.getLogger().info("[DR-DBG] Prevented item drop for "
-            + event.getPlayer().getName()
-            + " item=" + event.getItemDrop().getItemStack().getType());
-    }
-
-    @EventHandler(priority = MONITOR)
-    public void onPlayerItemSwap(
-            @NotNull PlayerSwapHandItemsEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getPlayer()))
-            return;
-
         event.setCancelled(true);
     }
 
     @EventHandler(priority = MONITOR)
-    public void onPlayerArrowPickup(
-            @NotNull PlayerPickupArrowEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getPlayer()))
+    public void onItemDrop(@NotNull PlayerDropItemEvent event) {
+        Player player = event.getPlayer();
+        if (!this.inDeathRun(player) || this.hasInventoryBypass(player) || player.isDead())
             return;
+        event.setCancelled(true);
+        this.plugin.getLogger().fine("[DeathRun] Prevented arena item drop for " + player.getName());
+    }
 
+    @EventHandler(priority = MONITOR)
+    public void onPlayerItemSwap(@NotNull PlayerSwapHandItemsEvent event) {
+        if (!this.inDeathRun(event.getPlayer()) || this.hasInventoryBypass(event.getPlayer()))
+            return;
         event.setCancelled(true);
     }
 
     @EventHandler(priority = MONITOR)
-    public void onPlayerItemPickup(
-            @NotNull PlayerAttemptPickupItemEvent event
-    ) {
-        if (this.hasInventoryBypass(event.getPlayer()))
+    public void onPlayerArrowPickup(@NotNull PlayerPickupArrowEvent event) {
+        if (!this.inDeathRun(event.getPlayer()) || this.hasInventoryBypass(event.getPlayer()))
             return;
-
         event.setCancelled(true);
     }
 
-    private boolean hasInventoryBypass(
-            @NotNull org.bukkit.command.CommandSender commandSender
-    ) {
+    @EventHandler(priority = MONITOR)
+    public void onPlayerItemPickup(@NotNull PlayerAttemptPickupItemEvent event) {
+        if (!this.inDeathRun(event.getPlayer()) || this.hasInventoryBypass(event.getPlayer()))
+            return;
+        event.setCancelled(true);
+    }
+
+    private boolean inDeathRun(@NotNull Player player) {
+        return this.arenaManager.runtimeForPlayer(player) != null;
+    }
+
+    private boolean hasInventoryBypass(@NotNull org.bukkit.command.CommandSender commandSender) {
         return commandSender.isOp() || commandSender.hasPermission(INVENTORY_BYPASS_PERMISSION);
     }
-
 }
