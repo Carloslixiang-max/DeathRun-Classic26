3a867f8f62b1571f20d3d7ae6c4cadeae0328202
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 16:56:57 +0800

    fix: isolate DeathRun listeners to its runtime

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinKickListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinKickListener.java
@@ -1,38 +1,9 @@
 package pl.mrstudios.deathrun.arena.listener;
 
-import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerLoginEvent;
-import org.jetbrains.annotations.NotNull;
 import pl.mrstudios.commons.inject.annotation.Inject;
 
-import static org.bukkit.event.EventPriority.MONITOR;
-import static org.bukkit.event.player.PlayerLoginEvent.Result.KICK_FULL;
-
+/** Intentionally empty. DeathRun must never override global login decisions. */
 public class ArenaPlayerJoinKickListener implements Listener {
-
-    @Inject
-    public ArenaPlayerJoinKickListener() {}
-
-    @EventHandler(priority = MONITOR)
-    public void onPlayerKick(
-            @NotNull PlayerLoginEvent event
-    ) {
-
-        if (event.getResult() == KICK_FULL)
-            if (event.getPlayer().hasPermission("mrstudios.deathrun.admin"))
-                event.allow();
-
-    }
-
-    @SuppressWarnings("deprecation")
-    @EventHandler(priority = MONITOR)
-    public void onPlayerLogin(
-            @NotNull PlayerLoginEvent event
-    ) {
-        if (event.getPlayer().hasPermission("mrstudios.deathrun.admin"))
-            event.allow();
-
-    }
-
+    @Inject public ArenaPlayerJoinKickListener() {}
 }
