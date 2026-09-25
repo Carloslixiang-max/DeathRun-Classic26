37ef3bd3f4b5083874ecdc0e139686335baf8ac6
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 16:57:17 +0800

    fix: isolate DeathRun listeners to its runtime

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaWorldListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaWorldListener.java
@@ -7,33 +7,34 @@ import org.bukkit.event.world.WorldSaveEvent;
 import org.bukkit.event.world.WorldUnloadEvent;
 import org.jetbrains.annotations.NotNull;
 import pl.mrstudios.commons.inject.annotation.Inject;
+import pl.mrstudios.deathrun.arena.ArenaManager;
 
 import static org.bukkit.event.EventPriority.MONITOR;
 
 public class ArenaWorldListener implements Listener {
 
+    private final ArenaManager arenaManager;
+
     @Inject
-    public ArenaWorldListener() {}
-
-    @EventHandler(priority = MONITOR)
-    public void onArenaWorldLoad(
-            @NotNull WorldLoadEvent event
-    ) {
-        event.getWorld().setAutoSave(false);
+    public ArenaWorldListener(@NotNull ArenaManager arenaManager) {
+        this.arenaManager = arenaManager;
     }
 
     @EventHandler(priority = MONITOR)
-    public void onWorldUnload(
-            @NotNull WorldUnloadEvent event
-    ) {
-        event.getWorld().setAutoSave(false);
+    public void onArenaWorldLoad(@NotNull WorldLoadEvent event) {
+        if (this.arenaManager.isDeathRunWorld(event.getWorld()))
+            event.getWorld().setAutoSave(false);
     }
 
     @EventHandler(priority = MONITOR)
-    public void onWorldSave(
-            @NotNull WorldSaveEvent event
-    ) {
-        event.getWorld().setAutoSave(false);
+    public void onWorldUnload(@NotNull WorldUnloadEvent event) {
+        if (this.arenaManager.isDeathRunWorld(event.getWorld()))
+            event.getWorld().setAutoSave(false);
     }
 
+    @EventHandler(priority = MONITOR)
+    public void onWorldSave(@NotNull WorldSaveEvent event) {
+        if (this.arenaManager.isDeathRunWorld(event.getWorld()))
+            event.getWorld().setAutoSave(false);
+    }
 }
