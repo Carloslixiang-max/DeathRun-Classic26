615f4d379ea2af3cc1204a499614adb8a0f13de1
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 16:57:02 +0800

    fix: isolate DeathRun listeners to its runtime

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaProjectileListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaProjectileListener.java
@@ -5,19 +5,23 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.jetbrains.annotations.NotNull;
 import pl.mrstudios.commons.inject.annotation.Inject;
+import pl.mrstudios.deathrun.arena.ArenaManager;
 
 import static org.bukkit.event.EventPriority.MONITOR;
 
 public class ArenaProjectileListener implements Listener {
 
-    @Inject
-    public ArenaProjectileListener() {}
+    private final ArenaManager arenaManager;
 
-    @EventHandler(priority = MONITOR)
-    public void onProjectileLand(
-            @NotNull ProjectileHitEvent event
-    ) {
-        event.getEntity().remove();
+    @Inject
+    public ArenaProjectileListener(@NotNull ArenaManager arenaManager) {
+        this.arenaManager = arenaManager;
     }
 
+    @EventHandler(priority = MONITOR)
+    public void onProjectileLand(@NotNull ProjectileHitEvent event) {
+        if (!this.arenaManager.isDeathRunWorld(event.getEntity().getWorld()))
+            return;
+        event.getEntity().remove();
+    }
 }
