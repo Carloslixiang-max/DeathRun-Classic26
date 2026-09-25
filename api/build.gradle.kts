df43cb3bf3f0dc5b4ffa1df14b014c9d9706a578
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 17:06:07 +0800

    build: simplify API module for Gradle 9

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -1,22 +1,12 @@
 plugins {
     id("java")
-    id("net.kyori.blossom") version "1.3.1"
-    id("com.gradleup.shadow") version "9.6.1"
 }
-
-val manualVersion = "1.4.1-classic26-dev"
 
 project.group = project.parent?.group!!
 project.version = project.parent?.version!!
 
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(25))
-}
-
-blossom {
-    replaceToken("{version}", manualVersion)
-    replaceToken("{gitBranch}", "classic26-dev")
-    replaceToken("{gitCommitHash}", "github-actions")
 }
 
 repositories {
@@ -30,17 +20,7 @@ dependencies {
     annotationProcessor("org.jetbrains:annotations:${project.parent?.property("jetbrains.annotations.version")}")
 }
 
-tasks {
-    withType<JavaCompile> {
-        options.encoding = "UTF-8"
-        options.release.set(25)
-    }
-
-    shadowJar {
-        archiveFileName.set("${project.parent?.name}-${project.name}-${project.version}.jar")
-    }
-
-    build {
-        dependsOn(shadowJar)
-    }
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.release.set(25)
 }
