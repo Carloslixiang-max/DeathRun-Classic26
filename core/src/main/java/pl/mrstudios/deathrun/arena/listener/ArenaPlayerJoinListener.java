f91e1650c47a9d86f800e2661710abc4e8378d9b
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 16:56:52 +0800

    fix: isolate DeathRun listeners to its runtime

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinListener.java
@@ -1,56 +1,27 @@
 package pl.mrstudios.deathrun.arena.listener;
 
-import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.potion.PotionEffect;
 import org.jetbrains.annotations.NotNull;
 import pl.mrstudios.commons.inject.annotation.Inject;
 import pl.mrstudios.deathrun.arena.ArenaManager;
 
-import static java.lang.Integer.MAX_VALUE;
-import static org.bukkit.GameMode.ADVENTURE;
 import static org.bukkit.event.EventPriority.MONITOR;
-import static org.bukkit.potion.PotionEffectType.NIGHT_VISION;
-import static org.bukkit.potion.PotionEffectType.SATURATION;
 
+/** Main-server-safe join listener: it never clears or rewrites unrelated player state. */
 public class ArenaPlayerJoinListener implements Listener {
 
-        private final ArenaManager arenaManager;
-    private final BukkitAudiences audiences;
+    private final ArenaManager arenaManager;
 
     @Inject
-    public ArenaPlayerJoinListener(
-                        @NotNull ArenaManager arenaManager,
-                    @NotNull BukkitAudiences audiences
-    ) {
-                this.arenaManager = arenaManager;
-        this.audiences = audiences;
+    public ArenaPlayerJoinListener(@NotNull ArenaManager arenaManager) {
+        this.arenaManager = arenaManager;
     }
 
-    @SuppressWarnings("deprecation")
     @EventHandler(priority = MONITOR)
-    public void onPlayerJoin(
-            @NotNull PlayerJoinEvent event
-    ) {
-
-        event.setJoinMessage("");
-
-        event.getPlayer().getActivePotionEffects()
-                .stream()
-                .map(PotionEffect::getType)
-                .forEach(event.getPlayer()::removePotionEffect);
-
-        event.getPlayer().getInventory().clear();
-        event.getPlayer().setGameMode(ADVENTURE);
-        event.getPlayer().addPotionEffect(new PotionEffect(SATURATION, MAX_VALUE, 1, false, false, false));
-        event.getPlayer().addPotionEffect(new PotionEffect(NIGHT_VISION, MAX_VALUE, 1, false, false, false));
-
+    public void onPlayerJoin(@NotNull PlayerJoinEvent event) {
+        this.arenaManager.restorePendingSnapshot(event.getPlayer());
         this.arenaManager.recoverPlayerToHubIfNeeded(event.getPlayer(), true);
-
-        this.audiences.player(event.getPlayer()).sendMessage(net.kyori.adventure.text.minimessage.MiniMessage.miniMessage().deserialize("<gold>[DR]</gold> <gray>Use <white>/deathrun maps <gray>or queue signs to join a map."));
-
     }
-
 }
