1693b6c6e3ade2b99ae8c495f44d2bc8725c1982
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 17:00:56 +0800

    feat: add Classic26 runtime services

---
+++ core/src/main/java/pl/mrstudios/deathrun/classic/death/DeathRunDeathCause.java
@@ -0,0 +1,13 @@
+package pl.mrstudios.deathrun.classic.death;
+
+public enum DeathRunDeathCause {
+    DIRECT_DAMAGE,
+    FALL,
+    FIRE,
+    WATER,
+    LAVA,
+    VOID,
+    TRAP,
+    SCRIPTED,
+    UNKNOWN
+}
