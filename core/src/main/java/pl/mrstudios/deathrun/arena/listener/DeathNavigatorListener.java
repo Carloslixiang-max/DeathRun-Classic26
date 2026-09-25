fa0918a12372739b5b18a2e0329a0158344834d3
Carloslixiang-max <Carlos.lixiang@hotmail.com>
2026-09-25 17:01:07 +0800

    feat: add Classic26 runtime services

---
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/DeathNavigatorListener.java
@@ -0,0 +1,63 @@
+package pl.mrstudios.deathrun.arena.listener;
+
+import org.bukkit.Server;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.Action;
+import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.plugin.Plugin;
+import org.jetbrains.annotations.NotNull;
+import pl.mrstudios.commons.inject.annotation.Inject;
+import pl.mrstudios.deathrun.api.arena.user.IUser;
+import pl.mrstudios.deathrun.arena.ArenaManager;
+import pl.mrstudios.deathrun.classic.death.DeathNavigatorService;
+import pl.mrstudios.deathrun.classic.trap.TrapActivationService;
+import pl.mrstudios.deathrun.config.Configuration;
+
+import static pl.mrstudios.deathrun.api.arena.enums.GameState.PLAYING;
+import static pl.mrstudios.deathrun.api.arena.user.enums.Role.DEATH;
+
+public final class DeathNavigatorListener implements Listener {
+
+    private final ArenaManager arenaManager;
+    private final DeathNavigatorService navigator;
+    private final TrapActivationService activationService;
+
+    @Inject
+    public DeathNavigatorListener(
+            @NotNull ArenaManager arenaManager,
+            @NotNull Plugin plugin,
+            @NotNull Server server,
+            @NotNull Configuration configuration
+    ) {
+        this.arenaManager = arenaManager;
+        this.navigator = new DeathNavigatorService(plugin);
+        this.activationService = new TrapActivationService(plugin, server, configuration);
+    }
+
+    @EventHandler
+    public void onInteract(@NotNull PlayerInteractEvent event) {
+        if (event.getAction() != Action.RIGHT_CLICK_AIR && event.getAction() != Action.RIGHT_CLICK_BLOCK)
+            return;
+        ArenaManager.ArenaRuntime runtime = this.arenaManager.runtimeForPlayer(event.getPlayer());
+        if (runtime == null || runtime.arena().getGameState() != PLAYING)
+            return;
+        IUser user = runtime.arena().getUser(event.getPlayer());
+        if (user == null || user.getRole() != DEATH)
+            return;
+        DeathNavigatorService.Action action = this.navigator.actionOf(event.getItem());
+        if (action == null)
+            return;
+        event.setCancelled(true);
+        switch (action) {
+            case PREVIOUS -> this.navigator.previous(event.getPlayer(), runtime);
+            case NEXT -> this.navigator.next(event.getPlayer(), runtime);
+            case JUMP -> this.navigator.jump(event.getPlayer(), runtime);
+            case ACTIVATE -> {
+                int index = this.navigator.selectedIndex(event.getPlayer(), runtime);
+                if (index >= 0)
+                    this.activationService.activate(event.getPlayer(), runtime, index);
+            }
+        }
+    }
+}
